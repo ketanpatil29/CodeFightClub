4966a24a2818c42a07c390ced240f46151d8ef49
Ketan Patil <ketanpatil.sh@gmail.com>
2025-09-13 22:25:50 +0530

    Modified header of CFC

--- src/components/HeaderTop.jsx
+++ src/components/HeaderTop.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 const HeaderTop = () => {
   return (
-    <div className="bg-gray-200 w-full flex flex-col items-center text-center p-3">
-        <h4 className="[font-family:'Space_Grotesk',sans-serif] text-lg">Welcome to,</h4>
-        <img src="/src/assets/cfc.gif" alt="CodeFightClub" className="h-24 object-contain" />
-        <h4 className="[font-family:'Space_Grotesk',sans-serif] text-lg">Engage in a head-to-head coding battle. Select your language, showcase your logic, and rise to the challenge.</h4>
-    </div>
+    <nav className="fixed w-full z-50 bg-white/90 backdrop-blur-sm border-b border-gray-100 py-4 px-6">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <img src="/src/assets/cfc.gif" alt="logo" className="h-9 w-auto" />
+        
+        <div className="hidden md:flex space-x-8">
+          <a className="text-gray-700 hover:scale-110 hover:text-purple-600 transition-transform duration-200 font-medium">HOME</a>
+          <a className="text-gray-700 hover:scale-110 hover:text-purple-600 transition-transform duration-200 font-medium">HOW IT WORKS?</a>
+          <a className="text-gray-700 hover:scale-110 hover:text-purple-600 transition-transform duration-200 font-medium">CATEGORIES</a>
+          <a className="text-gray-700 hover:scale-110 hover:text-purple-600 transition-transform duration-200 font-medium">LEADERBOARD</a>
+
+          <button className="text-pink-700 hover:scale-110 hover:text-purple-700 transition-transform duration-200 font-medium">LOGIN</button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
