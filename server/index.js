b24ed44ab871473e56d0457f0f83700f7804ba4b
Ketan patil <ketanpatil.sh@gmail.com>
2025-10-07 21:47:31 +0530

    Changes in backend

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import runCodeRouter from "./authentication/routes/runCode.js";
 import aiQuestionsRouter from "./aiQuestions.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5174", credentials: true }));
 app.use(express.json());
 
 // Routes
--- server/socketHandler.js
+++ server/socketHandler.js
@@ -7,7 +7,7 @@ const waitingQuestions ={};        // Store question per roomId
 
 export default function initSocket(server) {
   const io = new Server(server, {
-    cors: { origin: "http://localhost:5173", methods: ["GET", "POST"], credentials: true }
+    cors: { origin: "http://localhost:5174", methods: ["GET", "POST"], credentials: true }
   });
 
   io.on("connection", (socket) => {
