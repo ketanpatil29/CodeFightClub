ed712d8597c0d737ebc2eed6f30dc614c45df4d3
Ketan patil <ketanpatil.sh@gmail.com>
2025-10-08 21:24:14 +0530

    updated arena

--- src/components/Arena.jsx
+++ src/components/Arena.jsx
@@ -20,7 +20,7 @@ const Arena = ({ user, opponentName, question, onExit }) => {
     const roomId = localStorage.getItem("arenaData")?.roomId;
 
     try {
-      const res = await fetch("http://localhost:5000/submit-answer", {
+      const res = await fetch("http://localhost:3000/submit-answer", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code, question }),
