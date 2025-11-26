9e343548f88741dcb8e5c8ce3e0d91677a434d04
Ketan patil <ketanpatil.sh@gmail.com>
2025-11-26 22:48:19 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import runCodeRouter from "./authentication/routes/runCode.js";
 import aiQuestionsRouter from "./aiQuestions.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 // Mount auth routes from server.js
