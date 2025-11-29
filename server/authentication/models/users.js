ed5e3c84f47655194e8cf2e7675759ed1dba4f75
Ketan patil <ketanpatil.sh@gmail.com>
2025-11-29 17:47:08 +0530

    update users.js

--- server/authentication/models/users.js
+++ server/authentication/models/users.js
@@ -2,11 +2,25 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
+
+  // For normal login
   passwordHash: { type: String },
+
+  // OTP login
   otp: { type: String },
   otpExpiry: { type: Date },
+
+  // OAuth fields
+  providers: {
+    type: [String],   // ["google", "github"]
+    default: [],
+  },
+
+  googleId: { type: String },
+  githubId: { type: String },
+  name: { type: String },
+  avatar: { type: String },
 });
 
-// ESM export
 const User = mongoose.model("User", userSchema);
 export default User;
