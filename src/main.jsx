eabb9369080fa7030777c9bd0bab58fef09a0c61
Ketan patil <ketanpatil.sh@gmail.com>
2025-12-04 08:25:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { SocketProvider } from './context/MatchContext'; // import it
+import { SocketProvider } from './context/MatchContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SocketProvider>
-      <App />
-    </SocketProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
