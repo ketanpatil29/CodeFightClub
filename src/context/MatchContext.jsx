d415f2f0ed5d3859eb0d25b5f64af7fdc50c87dc
Ketan patil <ketanpatil.sh@gmail.com>
2025-12-29 15:28:41 +0530

    Update urls

--- src/context/MatchContext.jsx
+++ src/context/MatchContext.jsx
@@ -8,7 +8,7 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socketUrl = import.meta.env.VITE_SOCKET_URL;
+    const socketUrl = import.meta.env.VITE_BACKEND_URL;
 
     const newSocket = io(socketUrl, {
       withCredentials: true,
