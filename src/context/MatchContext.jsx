040a17f41bf7c927f4c23e1099393375750f0bdb
Ketan patil <ketanpatil.sh@gmail.com>
2025-12-29 15:52:58 +0530

    Update MatchContext.jsx

--- src/context/MatchContext.jsx
+++ src/context/MatchContext.jsx
@@ -8,7 +8,7 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socketUrl = import.meta.env.VITE_BACKEND_URL;
+    const socketUrl = import.meta.env.VITE_API_BASE;
 
     const newSocket = io(socketUrl, {
       withCredentials: true,
